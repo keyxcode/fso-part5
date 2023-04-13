8b078feb254830facdd8ddcba6743ec0d892db86
keyxcode <keyxcode@gmail.com>
2023-04-13 13:21:09 +0700

    Solution 5.5: create togglable for new note form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Blog from "./components/Blog";
 import LoginForm from "./components/LoginForm";
 import BlogForm from "./components/BlogForm";
 import Notification from "./components/Notification";
+import Togglable from "./components/Togglable";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 
@@ -99,15 +100,17 @@ const App = () => {
           <div>
             {user.name} logged in<button onClick={handleLogout}>logout</button>
           </div>
-          <BlogForm
-            title={title}
-            setTitle={setTitle}
-            author={author}
-            setAuthor={setAuthor}
-            url={url}
-            setUrl={setUrl}
-            handleCreateBlog={handleCreateBlog}
-          />
+          <Togglable buttonLabel="new note">
+            <BlogForm
+              title={title}
+              setTitle={setTitle}
+              author={author}
+              setAuthor={setAuthor}
+              url={url}
+              setUrl={setUrl}
+              handleCreateBlog={handleCreateBlog}
+            />
+          </Togglable>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
