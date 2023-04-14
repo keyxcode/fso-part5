7106019a364ba8df8b39f6bbf0070bb3113fd0bb
keyxcode <keyxcode@gmail.com>
2023-04-14 21:10:02 +0700

    Solution 5.18: write test for login

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -2,11 +2,45 @@
 
 describe("Blog app", function () {
   beforeEach(function () {
-    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.request("POST", `${Cypress.env("BACKEND")}/testing/reset`);
+
+    const user = {
+      username: "test",
+      name: "test_user",
+      password: "123",
+    };
+    cy.request("POST", `${Cypress.env("BACKEND")}/users`, user);
+
     cy.visit("");
   });
 
   it("login form is shown", function () {
     cy.contains("log in to application");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("test");
+      cy.get("#password").type("123");
+      cy.get("#login-button").click();
+
+      cy.contains("test_user logged in");
+    });
+
+    it("fails with wrong username", function () {
+      cy.get("#username").type("wronguser");
+      cy.get("#password").type("123");
+      cy.get("#login-button").click();
+
+      cy.contains("wrong user name or password");
+    });
+
+    it("fails with wrong password", function () {
+      cy.get("#username").type("test");
+      cy.get("#password").type("wrong password");
+      cy.get("#login-button").click();
+
+      cy.contains("wrong user name or password");
+    });
   });
 });
