da77dd0640d5ac74f3d5ca10d621bdc9906b9b39
keyxcode <keyxcode@gmail.com>
2023-04-14 21:01:20 +0700

    Solution 5.17: test that login form is shown by default

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,8 +1,12 @@
 /* eslint-disable prefer-arrow-callback, func-names */
 
 describe("Blog app", function () {
-  it("front page can be opened", function () {
+  beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
     cy.visit("");
+  });
+
+  it("login form is shown", function () {
     cy.contains("log in to application");
   });
 });
